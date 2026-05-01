2d8d708eba120c094b9d8d542c054606503a6ead
Fatih Bakal <fatihbakal7@protonmail.com>
2026-05-01 19:35:32 +0300

    Use GIT_HASH + GIT_DATE instead of CYPRESS_BUILDDATE

--- Server/Source/Core/VersionInfo.cpp
+++ Server/Source/Core/VersionInfo.cpp
@@ -1,9 +1,7 @@
 #include "pch.h"
 #include "VersionInfo.h"
 
-#define CYPRESS_BUILDDATE (__DATE__ " " __TIME__)
-
 std::string GetCypressVersion()
 {
-	return std::format("{}.{}.{} ({})", CYPRESS_VERSION_MAJOR, CYPRESS_VERSION_MINOR, CYPRESS_VERSION_PATCH, CYPRESS_BUILDDATE);
+	return std::format("{}.{}.{} ({}.{})", CYPRESS_VERSION_MAJOR, CYPRESS_VERSION_MINOR, CYPRESS_VERSION_PATCH, GIT_HASH, GIT_DATE);
 }
